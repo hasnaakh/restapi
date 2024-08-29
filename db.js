97df089182d73b780066c41dee12a9a0e1ccf34c
Yomna khaled <yomnakhaled2001@gmail.com>
2024-08-29 18:22:42 +0300

    second commit

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
+    user: "yomna",
     host: "localhost",
     database: "stestt",
-    password: "1234",
+    password: "123",
     port: 5432,
 });
 
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require('cors');
 const studentRoutes = require('./src/student/routes');
 
 const app = express();
 const port = 3000;
+app.use(cors());
 
 app.use(express.json());
 
